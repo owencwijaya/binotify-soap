5b2a460de9445a8147bb0537f21b8d3203214571
Muhammad Risqi Firdaus <risqi.firdaus20@gmail.com>
2022-11-21 13:44:56 +0700

    init: soap setup

---
+++ binotify-soap/src/main/java/com/binotify/TesServlet.java
@@ -0,0 +1,12 @@
+package com.binotify;
+
+import jakarta.jws.WebService;
+import jakarta.jws.soap.SOAPBinding;
+ 
+@WebService
+@SOAPBinding(style = SOAPBinding.Style.RPC) 
+public class TesServlet {
+   public String ping() {
+       return "pong";
+   }
+}
